c1bbe063b435df7fe377ba2d1760cb9177b7e222
Kamidev <64328935+Kamrul-sys@users.noreply.github.com>
2026-03-29 21:38:03 +0200

    Update script.js
    test version

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-document.getElementById("startBtn").addEventListener("click", function () {
-    alert("Game button works!");
+let startBtn = document.getElementById("startBtn");
+let gameDiv = document.getElementById("game");
+
+let leftName = document.getElementById("leftName");
+let rightName = document.getElementById("rightName");
+
+let leftImg = document.getElementById("leftImg");
+let rightImg = document.getElementById("rightImg");
+
+let leftBtn = document.getElementById("leftBtn");
+let rightBtn = document.getElementById("rightBtn");
+
+let resultText = document.getElementById("result");
+
+startBtn.addEventListener("click", function () {
+    gameDiv.style.display = "block";
+    startBtn.style.display = "none";
+
+    leftName.innerHTML = "Bulbasaur";
+    rightName.innerHTML = "Charmander";
+
+    leftImg.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png";
+    rightImg.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png";
+
+    resultText.innerHTML = "Test round loaded.";
+});
+
+leftBtn.addEventListener("click", function () {
+    resultText.innerHTML = "You clicked Left.";
+});
+
+rightBtn.addEventListener("click", function () {
+    resultText.innerHTML = "You clicked Right.";
 });
